07771931ccee22c57cb9d2c9672de079e3ceb813
sompaak <aksompalli@gmail.com>
2019-09-11 19:31:50 -0700

    added creds

--- provider.tf
+++ provider.tf
@@ -5,5 +5,5 @@ provider "google" {
     project = "gold-pod-250515"
     region  = "us-central1"
     zone    = "us-central1-c"
-    credentials= "/Users/akhil/downloads/gold-pod-250515-242a8109f361.json"
+    credentials= "./gold-pod-250515-242a8109f361.json"
 }
